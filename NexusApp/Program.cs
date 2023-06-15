a7ce0b449ecfb7d2235bebcf7b391395939f9d50
nguyenhongquan <lactuong64@gmail.com>
2023-06-15 20:14:01 +0700

    First Update Database

--- NexusApp/Program.cs
+++ NexusApp/Program.cs
@@ -98,21 +98,6 @@ builder.Services.AddCors(options =>
             policy.AllowAnyMethod();
         });
 });
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-{
-    /*options.RequireHttpsMetadata = false;*/
-    options.SaveToken = true;
-    options.TokenValidationParameters = new TokenValidationParameters()
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        IssuerSigningKey = new SymmetricSecurityKey
-        (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-    };
-});
-
 var app = builder.Build();
 app.UseCors();
 // Configure the HTTP request pipeline.
